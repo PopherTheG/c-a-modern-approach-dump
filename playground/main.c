688c15ad1c7d2fc3f329054b89fafa59fe04b337
Mark Christopher C. Gemzon <markchristopher.gemzon@Marks-MacBook-Pro.local>
2022-03-16 23:12:50 +0800

    hello hello hello

--- playground/main.c
+++ playground/main.c
@@ -2,10 +2,17 @@
 #include <string.h>
 #include <stdlib.h>
 
+void make_p_a_point_to_b(char **p_ch, char * p_new) {
+    *p_ch = p_new;    
+}
+
 int main(int argc, char *argv[]) {
-    char *p = malloc(4);
-    free(p);
-    strcpy(p, "abc");
-    printf("%s\n", p);
+    char a = 'a';
+    char b = 'b';
+    
+    char * p_a = &a;
+    printf("%c\n", *p_a);
+    make_p_a_point_to_b(&p_a, &b);
+    printf("%c\n", *p_a);
     return 0;
 }
